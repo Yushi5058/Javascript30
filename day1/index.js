0c2abe6398755d381b4fc28059ddbf64908d3569
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-03-22 13:35:35 +0000

    update js file

--- day1/index.js
+++ day1/index.js
@@ -3,9 +3,19 @@ function playSound(e) {
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from running 
 
-    key.classList.toggle('playing');
+    key.classList.add('playing');
     audio.currentTime = 0; // rewind to the start;
     audio.play();
 }
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip this if it's not a 
+    // transform 
+    this.classList.remove('playing');
+
+}
+const keys = document.querySelectorAll('.keys');
 window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
