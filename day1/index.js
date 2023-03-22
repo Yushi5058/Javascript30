3b7b09b32364028a4b61add30ab6b2285fe9a27b
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-03-22 12:58:27 +0000

    update files

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,12 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.eventCode}"]`);
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from running 
+
+    key.classList.add('playing');
     audio.currentTime = 0; // rewind to the start;
     audio.play();
-})
+}
+
+const keys = document.querySelectorAll('.keys');
+window.addEventListener('keydown', playSound);
