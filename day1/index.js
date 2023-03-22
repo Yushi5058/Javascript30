027c92420e06fbb6837a6a43cae55583dbd5d970
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-03-22 12:05:29 +0000

    update files

--- day1/index.js
+++ day1/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.eventCode}"]`);
     if(!audio) return; // stop the function from running 
     audio.currentTime = 0; // rewind to the start;
     audio.play();
