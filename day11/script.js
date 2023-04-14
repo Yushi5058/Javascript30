c9ea61db078370d9bd8afc5496c45ef686ee28c7
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-04-14 20:14:18 +0000

    update script file

--- day11/script.js
+++ day11/script.js
@@ -9,7 +9,19 @@ const ranges = player.querySelectorAll('.player__slider');
 
 
 // Build our functions 
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
 
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
 
 
 // Hook up the event listeners 
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
